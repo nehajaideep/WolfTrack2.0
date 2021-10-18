3a8f1e054a72d90c63ec0cf69ee058e7407f3433
aditya4196 <32817064+aditya4196@users.noreply.github.com>
2021-10-17 23:53:22 -0400

    Resume PDF reader function

---
+++ Backend/ResumeParser.py
@@ -0,0 +1,35 @@
+import io
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfpage import PDFPage
+#Docx resume
+import docx2txt
+#Wordcloud
+import re
+import operator
+from nltk.tokenize import word_tokenize 
+from nltk.corpus import stopwords
+set(stopwords.words('english'))
+from wordcloud import WordCloud
+from nltk.probability import FreqDist
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def read_pdf_resume(pdf_doc):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle)
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+    with open(pdf_doc, 'rb') as fh:
+        for page in PDFPage.get_pages(fh, caching=True,check_extractable=True):           
+            page_interpreter.process_page(page)     
+        text = fake_file_handle.getvalue() 
+    # close open handles      
+    converter.close() 
+    fake_file_handle.close() 
+    if text:     
+        return text
+
+
